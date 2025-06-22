9696134e8947d121d42e97c59bc0553c35467e24
duckie <sarthakmisraa@gmail.com>
2025-06-22 14:58:14 +0530

    new website

--- postcss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    plugins: [
-        require('postcss-preset-env'),
-    ],
-};
-
-// "sass": "sass assets/scss/style.scss assets/styles/style.css -w"
-// "postcss": "postcss assets/styles/style.css -o assets/styles/style.css -w"
